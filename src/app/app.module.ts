8c051f4cae0057c6884b2402f58ee929775f0e97
Junaid <junaid.alam@gds.ey.com>
2025-05-14 23:27:40 +0530

    changes to import { ChatComponent } from './chat/chat.component';

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
+
+
+import { AppComponent } from './app.component';
+
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
